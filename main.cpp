0178c94c51f952fad38eebcdd2792c3be38cb101
PauloCesarMS <genesis45.ppc@gmail.com>
2021-03-16 20:28:51 -0300

    Test commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include "ADTs/Prim.hpp"
+#include "ADTs/Kruskal.hpp"
+
+#include <iostream>
+
+int main()
+{
+    for(unsigned int i = 0; i < 100000; i++)
+        std::cout << "Lel" << std::endl;
+
+    return 0;
+}
