43690227ddef94762c262aefce10c8e6db0f7dff
PauloCesarMS <genesis45.ppc@gmail.com>
2021-03-20 15:19:11 -0300

    Dynamic allocation, better file opening and closure, smaller fixes and improvements to the code

--- main.cpp
+++ main.cpp
@@ -3,52 +3,72 @@
 #include <cmath>
 #include <iostream>
 #include <fstream>
-#include <string.h>
+#include <string>
 
-float EucledeanDistance(float x1, float y1, float x2, float y2);
+float EuclideanDistance(float x1, float y1, float x2, float y2);
 
 int main()
 {
     std::ifstream data_file;
     std::ifstream classes_file;
-    data_file.open("./Files/dados.txt", ios::in);
-    classes_file.open("./Files/classes.txt", ios::in);
-
     std::string readline;
 
-    float pointxyval[788][2]; //[x][y]
-    float classes[788];
+    unsigned int numlines = 0;
+
+    data_file.open("./Files/dados.txt", ios::in);
+    while(getline(data_file, readline) )
+    {
+        numlines++;
+    }
+
+    data_file.close();
+
+    float pointxyval[numlines][2]; //[x][y]
+
+    float classes[numlines];
     int i=0;
     char *piece_data;
-    while(!data_file.eof()){
-        getline(data_file, readline);
+
+
+    data_file.open("./Files/dados.txt", ios::in);
+
+    while(getline(data_file, readline) )
+    {
+        //getline(data_file, readline);
         piece_data = strtok(&readline[0], "	");
         pointxyval[i][0] = atof(piece_data);
         piece_data = strtok(NULL, "	");
         pointxyval[i][1] = atof(piece_data);
         i++;
     }
+    data_file.close();
 
-    float aux = pointxyval[0][0];
+    //float aux = pointxyval[0][0];
     i=0;
-    while(!classes_file.eof()){
-        getline(classes_file, readline);
+
+    readline = "\0";
+    classes_file.open("./Files/classes.txt", ios::in);
+
+    while(getline(classes_file, readline) )
+    {
         classes[i] = atoi(&readline[0]);
         i++;
     }
-     pointxyval[0][0] = aux;
+    //pointxyval[0][0] = aux;
 
-    for (i=0; i<787; i++){
-        printf("distancia do ponto (%.2f, %.2f) ate o ponto (%.2f, %.2f) e: %.2f\n",pointxyval[i][0], pointxyval[i][1], pointxyval[i+1][0], pointxyval[i+1][1], EucledeanDistance(pointxyval[i][0], pointxyval[i][1], pointxyval[i+1][0], pointxyval[i+1][1]));
-    }
+    for (i=0; i<numlines-1; i++)
+        std::cout << "distance from (" << pointxyval[i][0] << ", " << pointxyval[i][1] << ") to (" << pointxyval[i+1][0] << ", " << pointxyval[i+1][1] << "): " << EuclideanDistance(pointxyval[i][0], pointxyval[i][1], pointxyval[i+1][0], pointxyval[i+1][1]) << std::endl;
+
     i = 0;
 
-    while(i<788){
-        std::cout<<"Valor classes: "<<classes[i]<<std::endl;
+    while(i < numlines)
+    {
+        std::cout << "Class value: " << classes[i] << std::endl;
         i++;
     }
 }
 
-float EucledeanDistance(float x1, float y1, float x2, float y2){
+float EuclideanDistance(float x1, float y1, float x2, float y2)
+{
     return sqrt(((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1)));
 }
