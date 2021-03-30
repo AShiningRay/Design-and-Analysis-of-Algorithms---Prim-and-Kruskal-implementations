e6c9cc648a04cfd2c8cdb1ecf42d1a353ffda88d
MatheusRFC <matheusr220998@gmail.com>
2021-03-30 08:34:17 -0300

    Last changes and organizations.

--- ADTs/Prim.hpp
+++ ADTs/Prim.hpp
@@ -28,7 +28,7 @@ class Graph_Prim
 
         void calculate_Prim(int k, int *classes) // Where Prim's Minimum Spanning Tree is calculated.
         {
-
+            float vector_dist[788];
             std::priority_queue < vertweightpair, std::vector <vertweightpair> , std::greater <vertweightpair> > priority_q;
 
             std::vector<float> vertex_key(vertex, ULONG_MAX); // Allocates a vector populated with the biggest value possible for a vertex's number
@@ -38,8 +38,8 @@ class Graph_Prim
             int origin = 0;
             vertex_key[origin] = 0;
             priority_q.push( std::make_pair(0, origin) );
-
-            while (!priority_q.empty())
+            int cont=0;
+            while (!priority_q.empty()) //Complexity O(E log V)
             {
                 int vert1 = priority_q.top().second;
                 priority_q.pop(); // Removes the current node from the priority queue.
@@ -50,6 +50,7 @@ class Graph_Prim
 
                 for (vertex_iterator = adjacency[vert1].begin(); vertex_iterator != adjacency[vert1].end(); ++vertex_iterator)
                 {
+                    cont++;
                     int vert2 = (*vertex_iterator).first; // Finds the second vertex of the pair
                     float weight = (*vertex_iterator).second; // Gets the weight of the edge between both vertexes
 
@@ -62,100 +63,72 @@ class Graph_Prim
                 }
             }
 
-            for (int i = 1; i < vertex; ++i)
+
+            for (int i = 1; i < vertex; ++i){
                 std::cout << "Prim verts: " << parent_vertex[i] << " <-> " << i << " || " << vertex_key[i] << std::endl;
+                vector_dist[i] = vertex_key[i]; //Sotage the weights in a different array.
+            }
+            printf("VALOR %d\n", cont++);
 
-            int j = 1, i = 0, group = 0, groups[k];;
-            float tmp[k];
+            shellSort(vector_dist, 788); //Shellsort will sort the numbers in ascending order.
 
-            /*while(j < 788) //Function to unite vertices. PS: A binary heap has been not implemented yet to collect the k highest values.
+            float biggest[k-1]; //Creates an array to storage the biggest edges.
+            int t=0;
+            int i = 787;
+
+            //Finds and storages the buggest edges in the end of the array sorted by shellsort. Complexity O(k - 1)
+            while(i > (787 - (k-1))){
+                biggest[t] = vector_dist[i];
+                t++;
+                i--;
+            }
+
+            t = 0;
+            int j=0;
+            float valor;
+
+            while(j < 788) //Function to unite vertices. Complexity ((v - 1) * (k - 1))
             {
-                float tmp = 0;
-
-                if (vertex_key[j] >= 1.27)
+                valor = vertex_key[j];
+                if (check(valor, k-1, biggest) == 1) //Send the values to check() function compares if vertex_key[j] is one of the biggest edges.
                 {
                     classes[j-1] = t;
                     t++;
                 }
                 classes[j] = t;
-                i++;
-            }*/
+                j++;
+            }
+            write_results_file(classes); //Wirte results on a file
+        }
 
-            for (i = 0; i < k; i++)
-            {
-                tmp[i] = -1.0f;
-                j = 1;
-                while(j < 788) //Function to get the edges with the biggest cost.
-                {
-                    if(i == 0)
-                    {
-                        if (vertex_key[j] >= tmp[i])
-                        {
-                            tmp[i] = vertex_key[j];
-                        }
+        void shellSort(float *vet, int size_vet) { //Complexity does not defined.
+            int i , j;
+            float value_aux;
+            int t = 1;
+            while(t < size_vet) {
+                t = 3*t+1;
+            }
+            while ( t > 1) {
+                t /= 3;
+                for(i = t; i < size_vet; i++) {
+                    value_aux = vet[i];
+                    j = i - t;
+                    while (j >= 0 && value_aux < vet[j]) {
+                    vet [j + t] = vet[j];
+                    j -= t;
                     }
-                    else
-                    {
-                        if (vertex_key[j] >= tmp[i] && vertex_key[j] < tmp[i-1])
-                        {
-                            tmp[i] = vertex_key[j];
-                        }
-                    }
-                    j++;
+                    vet [j + t] = value_aux;
                 }
             }
+        }
 
-            printf("\n\n\n\n");
-
-            for(int i = k-1; i >= 0; i--)
-            { // prints the edges with biggest weights
-                printf("%f, ", tmp[i]);
+        //Function that will check if a number of vertex_key is equal to some of value of k. Complexity O(k - 1)
+        int check(float valor, int k, float *biggest){
+            for (int i=0; i<k; i++){
+                if (valor == biggest[i])
+                    return 1;
             }
-
-
-
-            for (i = 0; i < k; i++)
-            {
-                j = 1;
-                while(j < 788) //Function to find the positions in which the biggest cost edges are located.
-                {
-                    if (vertex_key[j] == tmp[k-1 - i])
-                    {
-                        classes[j-1] = group;
-                        groups[i] = j-1;
-                        group++;
-                    }
-                    classes[j] = group;
-                    j++;
-                }
-            }
-
-            for(i = 1; i < k; i++)
-            {   // Orders the classes's positions on the array.
-                int tmp;
-                if(groups[i-1] > groups[i])
-                {
-                    tmp = groups[i];
-                    groups[i] = groups[i-1];
-                    groups[i-1] = tmp;
-                }
-            }
-
-            for(i = 0; i < k; i++)
-                printf("\n%d, ", groups[i]);
-
-            for(int j = 0; j < k-1; j++)
-                for(i = 0 + groups[j]; i < 788; i++) // Used to classify the vertices based on the positions found in the function above
-                {
-                    if(i >= groups[j] && i < groups[j+1])
-                        classes[i] = j+1;
-                    else if(i >= groups[j])
-                        classes[i] = j+2;
-                }
-
-
-
-            write_results_file(classes); // O(n)
+            return 0;
         }
 
         //Function to save the results in a txt file.
